73193b78bda4dfbac42057c1bf686848f228df4b
Delgermurun Purevkhuu <delgermurun@users.noreply.github.com>
2022-05-09 17:01:17 +0200

    fix: added template into package_data

--- setup.py
+++ setup.py
@@ -92,4 +92,5 @@ if __name__ == '__main__':
         classifiers=_classifiers,
         project_urls=_project_urls,
         keywords=_keywords,
+        package_data={'jina-hubble-sdk': ['utils/template/logged_in_page.html']},
     )
