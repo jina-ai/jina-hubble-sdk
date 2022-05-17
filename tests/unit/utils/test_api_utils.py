e51212cbe2dcd7f2ca5ad882d4babb990cadf2ba
Zhaofeng Miao <522856232@qq.com>
2022-05-17 16:50:13 +0800

    test: fix get_base_url

--- tests/unit/utils/test_api_utils.py
+++ tests/unit/utils/test_api_utils.py
@@ -1,6 +1,9 @@
+import os
+
 from hubble.utils.api_utils import get_base_url
 
 
 def test_get_base_url():
     base_url = get_base_url()
-    assert base_url == 'https://api.hubble.jina.ai/v2/rpc/'
+    domain = os.environ['JINA_HUBBLE_REGISTRY']
+    assert base_url == f'{domain}/v2/rpc/'
