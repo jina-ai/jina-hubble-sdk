66d737ec92364362821754a2734906864b67e260
Wang Bo <bo.wang@jina.ai>
2022-06-06 11:44:03 +0200

    fix: asyncio login logout in notebook (#43)

--- hubble/__init__.py
+++ hubble/__init__.py
@@ -3,8 +3,12 @@ The Hubble Python Client
 """
 import asyncio
 
+import nest_asyncio
+
 from .client.client import Client  # noqa F401
 from .utils.auth import Auth  # noqa F401
+
+nest_asyncio.apply()
 
 
 def login():
