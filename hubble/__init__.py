b4f67bb43d94bb7510801112cf7a2b415f404adc
Zhaofeng Miao <522856232@qq.com>
2022-06-14 16:04:18 +0800

    fix(login): allow passing kwargs to login

--- hubble/__init__.py
+++ hubble/__init__.py
@@ -11,9 +11,9 @@ from .utils.auth import Auth  # noqa F401
 nest_asyncio.apply()
 
 
-def login():
+def login(**kwargs):
     """This function guide user to browser log-in and get token."""
-    asyncio.run(Auth.login())
+    asyncio.run(Auth.login(**kwargs))
 
 
 def logout():
