8339ace601bbc3344e64ba1f13ae340cb4bf240f
Zhaofeng Miao <522856232@qq.com>
2022-06-16 10:27:22 +0800

    test: clear env before some cases

--- tests/unit/client/test_client.py
+++ tests/unit/client/test_client.py
@@ -1,8 +1,12 @@
+import os
+from unittest.mock import patch
+
 import pytest
 from hubble import Client
 from hubble.excepts import AuthenticationRequiredError
 
 
+@patch.dict(os.environ, {"JINA_AUTH_TOKEN": ""})
 @pytest.mark.parametrize('params', [{'jsonify': True}, {'jsonify': False}])
 def test_handle_error_request(mocker, params):
     client = Client(**params)
