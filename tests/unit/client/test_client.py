efd14af58cf4cde4d718c887068e48074a35c9ab
Zhaofeng Miao <522856232@qq.com>
2022-06-16 10:13:45 +0800

    feat: change the way throw error
    Don't throw error in new Client procedure, if there
    is no authentication info. Instead, throw error when
    actually calling according API.
    
    Related to: https://github.com/jina-ai/auth/issues/2

--- tests/unit/client/test_client.py
+++ tests/unit/client/test_client.py
@@ -3,16 +3,10 @@ from hubble import Client
 from hubble.excepts import AuthenticationRequiredError
 
 
-def test_handle_error_request(mocker):
-    mocker.patch('hubble.utils.auth.Auth.get_auth_token', return_value='fake-token')
+@pytest.mark.parametrize('params', [{'jsonify': True}, {'jsonify': False}])
+def test_handle_error_request(mocker, params):
+    client = Client(**params)
+
+    mocker.patch('hubble.utils.auth.Auth.get_auth_token', return_value=None)
     with pytest.raises(AuthenticationRequiredError):
-        client = Client()
         client.get_user_info()
-
-
-def test_client_jsonify(mocker):
-    mocker.patch('hubble.utils.auth.Auth.get_auth_token', return_value='fake-token')
-    with pytest.raises(AuthenticationRequiredError):
-        client = Client(jsonify=True)
-        resp = client.get_user_info()
-        assert isinstance(resp, str)
--- tests/unit/utils/test_api_utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from hubble.utils.api_utils import get_base_url
-
-
-def test_get_base_url():
-    base_url = get_base_url()
-    domain = os.environ['JINA_HUBBLE_REGISTRY']
-    assert base_url == f'{domain}/v2/rpc/'
