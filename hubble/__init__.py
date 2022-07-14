fccecca777b094d19fbb8186035c1ed777fee526
bwanglzu <bo.wang@jina.ai>
2022-07-14 10:06:54 +0200

    refactor: remove nest asyncio

--- hubble/__init__.py
+++ hubble/__init__.py
@@ -3,12 +3,8 @@ The Hubble Python Client
 """
 import asyncio
 
-import nest_asyncio
-
 from .client.client import Client  # noqa F401
 from .utils.auth import Auth  # noqa F401
-
-nest_asyncio.apply()
 
 
 def login(**kwargs):
