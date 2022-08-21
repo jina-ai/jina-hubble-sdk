8464b51fe09d79b89a5e8b3ef91926cacb5584a5
Han Xiao <han.xiao@jina.ai>
2022-08-21 21:13:19 +0200

    refactor: unify release script with other jina prods

--- hubble/__init__.py
+++ hubble/__init__.py
@@ -6,13 +6,11 @@ import os
 from functools import wraps
 from typing import Optional
 
-import importlib_metadata
-
 from .client.client import Client  # noqa F401
 from .excepts import AuthenticationRequiredError
 from .utils.auth import Auth  # noqa F401
 
-__version__ = importlib_metadata.version('jina-hubble-sdk')
+__version__ = '0.15.0'
 
 
 def login_required(func):
