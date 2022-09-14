85c46a87b816ddbc8cd939b78103cddc3c8e26b2
Andrei Ungureanu <15995496+Andrei997@users.noreply.github.com>
2022-09-14 11:46:36 +0800

    feat(excepts): add maximum upload size reached error

--- hubble/excepts.py
+++ hubble/excepts.py
@@ -87,6 +87,10 @@ class InternalResourceIdConflictError(BaseError):
 
 class FileTooLargeError(BaseError):
     ...
+    
+
+class MaximumUploadSizeReachedError(BaseError):
+    ...
 
 
 class InternalDataCorruptionError(BaseError):
@@ -146,6 +150,7 @@ errorcodes = {
     40502: IncompatiableMethodError,
     40901: InternalResourceIdConflictError,
     41301: FileTooLargeError,
+    41302: MaximumUploadSizeReachedError
     42201: InternalDataCorruptionError,
     42202: IdentifierNamespaceOccupiedError,
     42203: SubmittedDataMalformedError,
