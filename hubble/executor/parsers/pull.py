4b3d56dc6cc91f3d45523916fd7341bf478ee4f1
yanlong.wang <yanlong.wang@naiver.org>
2023-01-10 12:35:31 +0800

    fix: move `--install-requirements` from hub pull options
    Jina pod is having its own version of mixin_hub_pull_options_parser.
    This aims to let them import from hubble

--- hubble/executor/parsers/pull.py
+++ hubble/executor/parsers/pull.py
@@ -9,12 +9,6 @@ def mixin_hub_pull_options_parser(parser):
 
     gp = add_arg_group(parser, title='Pull')
     gp.add_argument(
-        '--install-requirements',
-        action='store_true',
-        default=False,
-        help='If set, install `requirements.txt` in the Hub Executor bundle to local',
-    ),
-    gp.add_argument(
         '--force-update',
         '--force',
         action='store_true',
@@ -26,6 +20,8 @@ def mixin_hub_pull_options_parser(parser):
         type=str,
         help='The preferred target Docker platform. (e.g. "linux/amd64", "linux/arm64")',
     )
+
+    return gp
 
 
 def mixin_hub_pull_parser(parser):
@@ -44,4 +40,10 @@ def mixin_hub_pull_parser(parser):
         type=hub_uri,
         help='The URI of the executor to pull (e.g., jinaai[+docker]://<username>/NAME)',
     )
-    mixin_hub_pull_options_parser(parser)
+    pull_group = mixin_hub_pull_options_parser(parser)
+    pull_group.add_argument(
+        '--install-requirements',
+        action='store_true',
+        default=False,
+        help='If set, install `requirements.txt` in the Hub Executor bundle to local',
+    ),
